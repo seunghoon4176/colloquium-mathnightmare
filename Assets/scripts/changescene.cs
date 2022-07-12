700dc17edc38291f99de11c46087cb7e812d49a6
seunghoon4176 <sonseunghoon@icloud.com>
2022-07-12 22:21:17 +0900

    upload project

---
+++ Assets/scripts/changescene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonClick() //버튼 클릭 이벤트에 대한 함수를 만들어 준다.
+    { 
+        SceneManager.LoadScene("prolog");
+    }
+}
