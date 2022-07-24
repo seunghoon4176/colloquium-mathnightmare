d6f214ffecf8311e6907749d84044d91d1776f77
Junpyo Ko <kojunpyo2120@gmail.com>
2022-07-24 22:08:00 +0900

    Fight scene edit (dynamic)
    몬스터의 종류에 따라 다른 정보를 사용하도록 설정함.

---
+++ Assets/scripts/Fight.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 몬스터의 데이터 형식화
+public struct monster
+    {
+        public string name;
+        public string appearText;
+        public string info;
+        public string tip;
+        public int atk;
+        public int def;
+    }
+
+// 전투 씬 메인 클래스
+public class Fight : MonoBehaviour
+{
+    public Text appearText; // 출현 문구 저장할 변수 선언
+
+    void Start()
+    {
+        string monsterName = PlayerPrefs.GetString("monsterName"); // 전투 씬으로 넘어오기 전에 입력받은 몬스터의 이름을 가져옴 (change_to_fight 스크립트 참고)
+        string[] monsterNames = new string[]{"sin", "cos", "tan", "sincos", "costan", "sintan", "boss"}; // 배열에 몬스터 종류 저장
+        int monsterIndex = System.Array.IndexOf(monsterNames, monsterName); // 가져온 몬스터의 이름의 인덱스 값 저장
+        monster_setting(monsterIndex); // 인덱스 값을 인수로 주어 몬스터 설정을 실행하도록 함.
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // 각 몬스터의 정보를 저장하고, 인수에 들어온 값(= 몬스터 종류)에 따라 출현 문구 설정
+    public void monster_setting(int input_value)
+    {
+        // 각 몬스터의 정보 저장
+        monster sin;
+        sin.name = "Sin 클롭스";
+        sin.appearText = "Sin 클롭스가 나타났다!";
+        sin.info = "미친새끼.";
+        sin.tip = "Sin 함수의 개형은 ...";
+        sin.atk = 10;
+        sin.def = 8;
+
+        monster cos;
+        cos.name = "Cos 클롭스";
+        cos.appearText = "Cos 클롭스가 나타났다!";
+        cos.info = "또라이새끼.";
+        cos.tip = "Cos 함수의 개형은 ...";
+        cos.atk = 12;
+        cos.def = 10;
+
+        monster tan;
+        tan.name = "Tan 클롭스";
+        tan.appearText = "Tan 클롭스가 나타났다!";
+        tan.info = "개새끼.";
+        tan.tip = "tan 함수의 개형은 ...";
+        tan.atk = 15;
+        tan.def = 12;
+
+        monster sincos;
+        sincos.name = "SinCos 클롭스";
+        sincos.appearText = "SinCos 클롭스가 나타났다!";
+        sincos.info = "미친 또라이새끼.";
+        sincos.tip = "앞서 공부한 내용을 떠올리자.";
+        sincos.atk = 20;
+        sincos.def = 18;
+
+        monster costan;
+        costan.name = "CosTan 클롭스";
+        costan.appearText = "CosTan 클롭스가 나타났다!";
+        costan.info = "개또라이새끼.";
+        costan.tip = "앞서 공부한 내용을 떠올리자.";
+        costan.atk = 25;
+        costan.def = 20;
+
+        monster sintan;
+        sintan.name = "SinTan 클롭스";
+        sintan.appearText = "SinTan 클롭스가 나타났다!";
+        sintan.info = "개미친새끼.";
+        sintan.tip = "앞서 공부한 내용을 떠올리자.";
+        sintan.atk = 30;
+        sintan.def = 25;
+
+        monster boss;
+        boss.name = "보스";
+        boss.appearText = "보스가 나타났다!";
+        boss.info = "개미친 또라이새끼.";
+        boss.tip = "앞서 공부한 내용을 떠올리자.";
+        boss.atk = 100;
+        boss.def = 80;
+        
+        // 몬스터 종류에 따라 출현 문구 설정
+        switch(input_value) 
+        {
+            case 0 :
+                appearText.text = sin.appearText;
+                break;
+            case 1 :
+                appearText.text = cos.appearText;
+                break;
+            case 2 :
+                appearText.text = tan.appearText;
+                break;
+            case 3 :
+                appearText.text = sincos.appearText;
+                break;
+            case 4 :
+                appearText.text = costan.appearText;
+                break;
+            case 5 :
+                appearText.text = sintan.appearText;
+                break;
+            case 6 :
+                appearText.text = boss.appearText;
+                break;
+            }
+    }
+}
--- Assets/scripts/SceneChange/change_to_fight.cs
+++ Assets/scripts/SceneChange/change_to_fight.cs
@@ -17,8 +17,9 @@ public class change_to_fight : MonoBehaviour
         
     }
 
-    public void ButtonClick(string monster) //버튼 클릭 이벤트에 대한 함수를 만들어 준다.
+    public void ButtonClick(string monster) // 버튼(몬스터의 사진)을 클릭하면 실행. 함수 연결 과정에서 몬스터의 이름을 입력해주어야 함.
     { 
+        PlayerPrefs.SetString("monsterName", monster); // 몬스터의 이름을 씬이 넘어가도 사용 가능하도록 함. (일반적으로는 씬이 넘어갈 때 이전의 내용을 다 삭제해서 사용하지 못함)
         SceneManager.LoadScene("5. fight");
     }
 }
