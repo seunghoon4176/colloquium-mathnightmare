270f1c16602ee67e02c2c479eb046eb33c672472
Junpyo Ko <kojunpyo2120@naver.com>
2022-07-29 18:08:08 +0900

    캐릭터 이동 오류 수정, 복도 씬 에러 발생

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     float moveX, moveY;
-<<<<<<< HEAD
     float moveSpeed = 500f; // 이동 속도 500으로 설정. 에디터에서 1~500으로 설정 가능
 
-=======
-    [Header("이동 속도 조절")]
-    [SerializeField] [Range(1f, 500f)] float moveSpeed = 500f; // 이동 속도 500으로 설정. 에디터에서 1~500으로 설정 가능
->>>>>>> parent of 0eb4a88 (복도 배경 등록, 이동 일부 구현)
     void Update()
     {
         // 이동 (상하좌우 키: WASD키 혹은 상하좌우 키)
@@ -20,20 +15,12 @@ public class PlayerMovement : MonoBehaviour
 
         transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY); // 기존의 x값과 y값에 위에서 계산한 값을 더함.
 
-        // 카메라를 벗어나지 않도록 범위 제한
+        // 범위 제한
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-<<<<<<< HEAD
-        if (pos.x < -825) pos.x = -825;
-        if (pos.y < -380) pos.y = -380;
-        if (pos.x > 825) pos.x = 825;
-        if (pos.y > 245) pos.y = 245;
-=======
-        if (pos.x < 8f) pos.x = 8f;
-        if (pos.y < 15f) pos.y = 15f;
-        if (pos.x > 100f) pos.x = 100f;
-        if (pos.y > 80f) pos.y = 80f;
->>>>>>> parent of 0eb4a88 (복도 배경 등록, 이동 일부 구현)
+        if (pos.x < 6) pos.x = 6;
+        if (pos.y < 14) pos.y = 14;
+        if (pos.x > 102) pos.x = 102;
+        if (pos.y > 78) pos.y = 78;
         transform.position = Camera.main.ViewportToWorldPoint(pos);
-
     }
 }
--- Assets/scripts/mapScroll.cs
+++ Assets/scripts/mapScroll.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class mapScroll : MonoBehaviour
 {
+    [SerializeField] [Range(-5000f, 5000f)] float minX = 8f;
+    [SerializeField] [Range(-5000f, 5000f)] float maxX = 100f;
+    [SerializeField] [Range(0f, 500f)] float minY = 15f;
+    [SerializeField] [Range(0f, 500f)] float maxY = 80f;
+
     float moveX, moveY;
+    
     [Header("이동 속도 조절")]
-<<<<<<< HEAD
     [SerializeField] [Range(1f, 1000f)] float moveSpeed = 500f; // 이동 속도 500으로 설정. 에디터에서 1~1000으로 설정 가능
 
-=======
-    [SerializeField] [Range(1f, 100f)] float moveSpeed = 1000f; // 이동 속도 500으로 설정. 에디터에서 1~500으로 설정 가능
->>>>>>> parent of 0eb4a88 (복도 배경 등록, 이동 일부 구현)
     void Update()
     {
         // 이동 (상하좌우 키: WASD키 혹은 상하좌우 키)
         moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // 추가할 x값 계산
         moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; // 추가할 y값 계산
 
-        transform.position = new Vector2(transform.position.x - moveX, transform.position.y - moveY); // 기존의 x값과 y값에 위에서 계산한 값을 뺌.
+        transform.position = new Vector2(transform.position.x - moveX, transform.position.y - moveY); // 기존의 x값과 y값에 위에서 계산한 값을 더함.
+
+        // 범위 제한
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < minX) pos.x = minX;
+        if (pos.y < minY) pos.y = minY;
+        if (pos.x > maxX) pos.x = maxX;
+        if (pos.y > maxY) pos.y = maxY;
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
