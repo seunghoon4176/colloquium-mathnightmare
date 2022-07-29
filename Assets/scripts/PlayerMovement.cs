5799fb0c3b2594aa12b4dec3d995c45e37ded9af
Junpyo Ko <kojunpyo2120@naver.com>
2022-07-29 14:26:55 +0900

    교실 배경 등록, 캐릭터 이동 제한 설정

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -14,5 +14,14 @@ public class PlayerMovement : MonoBehaviour
         moveY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime; // 추가할 y값 계산
 
         transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY); // 기존의 x값과 y값에 위에서 계산한 값을 더함.
+
+        // 카메라를 벗어나지 않도록 범위 제한
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < 8f) pos.x = 8f;
+        if (pos.y < 15f) pos.y = 15f;
+        if (pos.x > 100f) pos.x = 100f;
+        if (pos.y > 80f) pos.y = 80f;
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
     }
 }
