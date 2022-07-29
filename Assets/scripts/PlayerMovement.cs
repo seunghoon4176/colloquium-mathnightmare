0eb4a88d975fd5a0db61f1f03597c5487657fac0
Junpyo Ko <kojunpyo2120@naver.com>
2022-07-29 15:06:06 +0900

    복도 배경 등록, 이동 일부 구현

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] [Range(1f, 500f)] float minX = 8f;
+    [SerializeField] [Range(1f, 10000f)] float maxX = 100f;
+    [SerializeField] [Range(1f, 500f)] float minY = 15f;
+    [SerializeField] [Range(1f, 500f)] float maxY = 80f;
+
     float moveX, moveY;
+    
     [Header("이동 속도 조절")]
     [SerializeField] [Range(1f, 500f)] float moveSpeed = 500f; // 이동 속도 500으로 설정. 에디터에서 1~500으로 설정 가능
+
     void Update()
     {
         // 이동 (상하좌우 키: WASD키 혹은 상하좌우 키)
@@ -15,13 +22,12 @@ public class PlayerMovement : MonoBehaviour
 
         transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY); // 기존의 x값과 y값에 위에서 계산한 값을 더함.
 
-        // 카메라를 벗어나지 않도록 범위 제한
+        // 범위 제한
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-        if (pos.x < 8f) pos.x = 8f;
-        if (pos.y < 15f) pos.y = 15f;
-        if (pos.x > 100f) pos.x = 100f;
-        if (pos.y > 80f) pos.y = 80f;
+        if (pos.x < minX) pos.x = minX;
+        if (pos.y < minY) pos.y = minY;
+        if (pos.x > maxX) pos.x = maxX;
+        if (pos.y > maxY) pos.y = maxY;
         transform.position = Camera.main.ViewportToWorldPoint(pos);
-
     }
 }
