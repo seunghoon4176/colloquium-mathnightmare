bf4c79af9fbe4506f29ab597c2e1db0eca7ccf2b
Junpyo Ko <kojunpyo2120@naver.com>
2022-08-04 16:21:18 +0900

    인벤토리 기본 처리 예제 첨삭
    이미지 파일 필요

--- Assets/scripts/inventorySystem/inventory.cs
+++ Assets/scripts/inventorySystem/inventory.cs
@@ -2,30 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class inventory : MonoBehaviour
+public class Inventory : MonoBehaviour
 {
-    bool alreadyInputed = false;
+    public List<Item> items;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        GameObject invUI = GameObject.Find("Canvas/inventorySystem/Bag");
-        invUI.SetActive(false);
+    [SerializeField]
+    private Transform slotParent;
+    [SerializeField]
+    private Slot[] slots;
+
+    private void OnValidate() {
+        slots = slotParent.GetComponentsInChildren<Slot>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        GameObject invUI = GameObject.Find("Canvas/inventorySystem/Bag");
-        
-        if (Input.GetKeyDown(KeyCode.I)) {
-            if (alreadyInputed == false) {
-                invUI.SetActive(true);
-                alreadyInputed = true;
-            } else if (alreadyInputed == true) {
-                invUI.SetActive(false);
-                alreadyInputed = false;
-            }
+    void Awake() {
+        FreshSlot();
+    }
+
+    public void FreshSlot() {
+        int i = 0;
+        for (; i < items.Count && i < slots.Length; i++) {
+            slots[i].item = items[i];
+        }
+        for (; i < slots.Length; i++) {
+            slots[i].item = null;
+        }
+    }
+
+    public void AddItem(Item _item) {
+        if (items.Count < slots.Length) {
+            items.Add(_item);
+            FreshSlot();
+        } else {
+            print("슬롯이 가득 차 있습니다.");
         }
     }
 }
