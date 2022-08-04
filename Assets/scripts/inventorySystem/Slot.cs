5e90a3d9e363177bd5d31c7ca98c9d45274ac188
Junpyo Ko <kojunpyo2120@naver.com>
2022-08-04 16:53:57 +0900

    아이템 에셋 생성

--- Assets/scripts/inventorySystem/Slot.cs
+++ Assets/scripts/inventorySystem/Slot.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
+    // SerializeField -> 인스펙터에서만 접근 가능하도록 할 때 사용.
+    // public을 사용하면 인스펙터와 다른 스크립트가 모두 접근 가능함.
+    // 스크립트에서는 접근을 못하지만 인스펙터에서는 접근 가능하게 할 때 SerializeField를 사용하며, 변수 앞에 대괄호로 적음.
     [SerializeField] Image image;
 
     private Item _item;
