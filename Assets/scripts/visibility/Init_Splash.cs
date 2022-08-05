54229e60c7a68772f6492b91f3054f50fa995cb0
Junpyo Ko <kojunpyo2120@naver.com>
2022-08-05 15:49:42 +0900

    광원 시스템 도입 시도

--- Assets/scripts/visibility/Init_Splash.cs
+++ Assets/scripts/visibility/Init_Splash.cs
@@ -3,41 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// 페이드 아웃 스크립트
 public class Init_Splash : MonoBehaviour
 
 {
-    GameObject SplashObj; // 판넬 오브젝트
-    Image image; // 판넬 이미지
+    GameObject SplashObj; // 패널 오브젝트
+    Image image; // 패널 이미지
 
-    [SerializeField] float delay;
+    [SerializeField] float delay; // 에디터에서 딜레이 조절 가능하도록 선언
 
     private bool checkbool = false; // 투명도 조절 논리형 변수 
 
     void Awake()
     {
-        SplashObj = this.gameObject; // 스크립트 참조된 오브젝트
-        image = SplashObj.GetComponent<Image>(); // 판넬 오브젝트에 이미지 참조
+        SplashObj = this.gameObject; // 스크립트 참조된 오브젝트를 패널 오브젝트에 대입
+        image = SplashObj.GetComponent<Image>(); // 패널 오브젝트에 이미지 참조
     }
 
     void Update()
     {
-        StartCoroutine("MainSplash"); // 코루틴 // 판넬 투명도 조절
+        StartCoroutine("MainSplash"); // 코루틴. 패널 투명도 조절
         if (checkbool) // 만약 checkbool이 참이면
         {
-            Destroy(this.gameObject); // 판넬 파괴, 삭제
+            Destroy(this.gameObject); // 패널 파괴, 삭제
         }
     } 
 
     IEnumerator MainSplash()
     {
-        Color color = image.color; // color에 판넬 이미지 참조
+        Color color = image.color; // color에 패널 이미지 참조
 
         for (int i = 100; i >= 0; i--) // for문 100번 반복 0보다 작을 때까지
         {
             color.a -= Time.deltaTime * delay; // 이미지 알파 값을 타임 델타 값 * 0.01
-            image.color = color; // 판넬 이미지 컬러에 바뀐 알파값 참조
+            image.color = color; // 패널 이미지 컬러에 바뀐 알파값 참조
 
-            if (image.color.a <= 0) // 만약 판넬 이미지 알파 값이 0보다 작으면
+            if (image.color.a <= 0) // 만약 패널 이미지 알파 값이 0보다 작으면
             {
                 checkbool = true; // checkbool 참 
             }
