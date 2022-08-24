d8621c8cdefacc18af45d4a81499ecd726ddfaf9
seunghoon4176 <ssh4176@naver.com>
2022-08-24 21:51:14 +0900

    junpyo i need help

--- Assets/scripts/visibility/imageVisibility.cs
+++ Assets/scripts/visibility/imageVisibility.cs
@@ -12,15 +12,22 @@ public class imageVisibility : MonoBehaviour
     }
 
     void Update() {
-        GameObject goImage = GameObject.Find("Canvas/textbookPage"); // 이미지 찾아서 변수에 등록
+        GameObject goImage = GameObject.Find("Canvas/textbookPage");
+        GameObject closebutton = GameObject.Find("Canvas/textbookPage/imageCloser");
+        GameObject problem = GameObject.Find("Canvas/textbookPage/imageCloser/problem"); // 이미지 찾아서 변수에 등록
         Color color = goImage.GetComponent<Image>().color; // 이미지의 색 추출
         
         if (PlayerPrefs.GetInt("clicked") == 1) { // textbookClick 스크립트에서 저장한 정보를 가져와서, 교과서쪽 버튼을 클릭했다면 실행
             color.a = 1f; // 불투명한 색으로 변경
             goImage.GetComponent<Image>().color = color; // 오브젝트에 반영
+            problem.GetComponent<Image>().color = color;
+            closebutton.GetComponent<Image>().color = color;
         } else if (PlayerPrefs.GetInt("clicked") == 0) { // 페이지를 닫는 버튼을 클릭했다면 실행
             color.a = 0f; // 투명화
             goImage.GetComponent<Image>().color = color; // 오브젝트에 반영
+            problem.GetComponent<Image>().color = color;
+            closebutton.GetComponent<Image>().color = color;
+            //아니 저 클로즈버튼 개새끼가 앞으로 오질 않아
         }
 
     }
