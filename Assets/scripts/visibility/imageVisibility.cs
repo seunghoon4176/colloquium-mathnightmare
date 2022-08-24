3bf75337ab7afbf3cb1fdf3533b14161e046c005
Junpyo Ko <kojunpyo2120@naver.com>
2022-08-24 23:41:14 +0900

    문제풀이 UI 추가

--- Assets/scripts/visibility/imageVisibility.cs
+++ Assets/scripts/visibility/imageVisibility.cs
@@ -14,7 +14,19 @@ public class imageVisibility : MonoBehaviour
     void Update() {
         GameObject goImage = GameObject.Find("Canvas/textbookPage");
         GameObject closebutton = GameObject.Find("Canvas/textbookPage/imageCloser");
-        GameObject problem = GameObject.Find("Canvas/textbookPage/imageCloser/problem"); // 이미지 찾아서 변수에 등록
+        GameObject problem = GameObject.Find("Canvas/textbookPage/problem"); // 이미지 찾아서 변수에 등록
+
+        GameObject AnswerInput = GameObject.Find("Canvas/textbookPage/AnswerInput");
+        GameObject SkipButton = GameObject.Find("Canvas/textbookPage/SkipButton");
+        GameObject ConfirmButton = GameObject.Find("Canvas/textbookPage/ConfirmButton");
+        GameObject WatchButton = GameObject.Find("Canvas/textbookPage/WatchButton");
+        GameObject explain = GameObject.Find("Canvas/textbookPage/explain");
+
+        GameObject AnswerInputText = GameObject.Find("Canvas/textbookPage/AnswerInput/TextArea/Text");
+        GameObject SkipButtonText = GameObject.Find("Canvas/textbookPage/SkipButton/Text");
+        GameObject ConfirmButtonText = GameObject.Find("Canvas/textbookPage/ConfirmButton/Text");
+        GameObject WatchButtonText = GameObject.Find("Canvas/textbookPage/WatchButton/Text");
+
         Color color = goImage.GetComponent<Image>().color; // 이미지의 색 추출
         
         if (PlayerPrefs.GetInt("clicked") == 1) { // textbookClick 스크립트에서 저장한 정보를 가져와서, 교과서쪽 버튼을 클릭했다면 실행
@@ -22,12 +34,33 @@ public class imageVisibility : MonoBehaviour
             goImage.GetComponent<Image>().color = color; // 오브젝트에 반영
             problem.GetComponent<Image>().color = color;
             closebutton.GetComponent<Image>().color = color;
+
+            AnswerInput.GetComponent<Image>().color = color;
+            SkipButton.GetComponent<Image>().color = color;
+            ConfirmButton.GetComponent<Image>().color = color;
+            WatchButton.GetComponent<Image>().color = color;
+            explain.GetComponent<Text>().color = color;
+
+            AnswerInputText.GetComponent<Text>().color = color;
+            SkipButtonText.GetComponent<Text>().color = color;
+            ConfirmButtonText.GetComponent<Text>().color = color;
+            WatchButtonText.GetComponent<Text>().color = color;
         } else if (PlayerPrefs.GetInt("clicked") == 0) { // 페이지를 닫는 버튼을 클릭했다면 실행
             color.a = 0f; // 투명화
             goImage.GetComponent<Image>().color = color; // 오브젝트에 반영
             problem.GetComponent<Image>().color = color;
             closebutton.GetComponent<Image>().color = color;
-            //아니 저 클로즈버튼 개새끼가 앞으로 오질 않아
+
+            AnswerInput.GetComponent<Image>().color = color;
+            SkipButton.GetComponent<Image>().color = color;
+            ConfirmButton.GetComponent<Image>().color = color;
+            WatchButton.GetComponent<Image>().color = color;
+            explain.GetComponent<Text>().color = color;
+ 
+            AnswerInputText.GetComponent<Text>().color = color;
+            SkipButtonText.GetComponent<Text>().color = color;
+            ConfirmButtonText.GetComponent<Text>().color = color;
+            WatchButtonText.GetComponent<Text>().color = color;
         }
 
     }
