c6003a959f6557e2fcc917952db2245da1f3dc81
seunghoon4176 <ssh4176@naver.com>
2022-09-08 12:01:09 +0900

    Update imageVisibility.cs

--- Assets/scripts/visibility/imageVisibility.cs
+++ Assets/scripts/visibility/imageVisibility.cs
@@ -6,23 +6,37 @@ using UnityEngine.UI;
 // 교과서 페이지 투명화 / 불투명화
 public class imageVisibility : MonoBehaviour
 {  
-    public Text lectures;
-    public Text confirm;
-    public Text skip;
-    public Text change;
+    public GameObject lectures;
+    public GameObject confirm;
+    public GameObject skip;
+    public GameObject change;
 
     // 맵 넘어갈 때, 게임 종료할 때 페이지를 닫지 않았어도 차후에 영향을 주지 않도록 프로그램 실행과 동시에 초기화
     void Awake() {
         PlayerPrefs.SetInt("clicked", 0);
+        /*
         lectures.text = "";
         confirm.text = "";
         skip.text = "";
         change.text = "";
+        */
 
-        // 요기는 이제 처음 실행하면 각  버튼을 x가 2000씩 옮겨진 코드를 넣어야 함.
+    
         
     }
      
+    void Start() {
+        
+        this.lectures = GameObject.Find("Canvas/textbookPage/WatchButton");
+        this.confirm = GameObject.Find("Canvas/textbookPage/ConfirmButton");
+        this.skip = GameObject.Find("Canvas/textbookPage/SkipButton");
+        this.change = GameObject.Find("Canvas/textbookPage/ChangeProblem"); //요 문제풀이의 버튼들을 다 움직이려고 가져옴.
+        
+        this.lectures.transform.position = new Vector2(1659,-445);
+        this.confirm.transform.position = new Vector2(1987,-445);
+        this.skip.transform.position = new Vector2(2315,-445);
+        this.change.transform.position = new Vector2(1659,-375);
+    }
     
     
 
@@ -30,13 +44,6 @@ public class imageVisibility : MonoBehaviour
         GameObject goImage = GameObject.Find("Canvas/textbookPage");
         GameObject closebutton = GameObject.Find("Canvas/textbookPage/imageCloser");
         GameObject problem = GameObject.Find("Canvas/textbookPage/problem"); // 이미지 찾아서 변수에 등록
-        
-        /*
-        GameObject lectures = GameObject.Find("Canvas/textbookPage/WatchButton");
-        GameObject confirm = GameObject.Find("Canvas/textbookPage/ConfirmButton");
-        GameObject skip = GameObject.Find("Canvas/textbookPage/SkipButton");
-        GameObject change = GameObject.Find("Canvas/textbookPage/ChangeProblem"); //요 문제풀이의 버튼들을 다 움직이려고 가져옴.
-        */
         
 
         //밑에꺼는 추후 input박스 만들려고 냅둠 일단 보류
@@ -51,13 +58,17 @@ public class imageVisibility : MonoBehaviour
             closebutton.GetComponent<Image>().color = color;
             //input.GetComponent<Image>().color = color;    
             
-            
+            /*
             lectures.text = "Watching lectures";
             confirm.text = "Confirm answer";
             skip.text = "Skip problem";
             change.text = "Change Problem";
-            
-    
+            */
+
+            this.lectures.transform.position = new Vector2(-341,-445);
+            this.confirm.transform.position = new Vector2(-13,-445);
+            this.skip.transform.position = new Vector2(315,-445);
+            this.change.transform.position = new Vector2(-341,-375);
         
         } else if (PlayerPrefs.GetInt("clicked") == 0) { // 페이지를 닫는 버튼을 클릭했다면 실행
             color.a = 0f; // 투명화
@@ -66,15 +77,19 @@ public class imageVisibility : MonoBehaviour
             closebutton.GetComponent<Image>().color = color;
             //input.GetComponent<Image>().color = color;    
 
-            
+            /*
             lectures.text = "";
             confirm.text = "";
             skip.text = "";
             change.text = ""; 
+            */
 
-            //닫았다면 다시 위에 awake에 들어가 있을, 얘네를 2000씩 미는 코드를 넣어야 함.
+            this.lectures.transform.position = new Vector2(1659,-445);
+            this.confirm.transform.position = new Vector2(1987,-445);
+            this.skip.transform.position = new Vector2(2315,-445);
+            this.change.transform.position = new Vector2(1659,-375);
         }
 
-        GameObject.Find("Canvas").transform.Find("director").gameObject.SetActive(true);
+        //GameObject.Find("Canvas").transform.Find("director").gameObject.SetActive(true);
     }
 }
